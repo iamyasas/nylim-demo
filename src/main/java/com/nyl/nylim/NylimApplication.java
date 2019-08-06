6e6a59ec9f8d73a83c3f191084797ea31d1be24c
Yasas Karunarathna <ykarunarathna@Virtusa.com>
2019-08-06 13:17:58 +0530

    Initial commit

---
+++ src/main/java/com/nyl/nylim/NylimApplication.java
@@ -0,0 +1,13 @@
+package com.nyl.nylim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NylimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NylimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyl/nylim/NylimApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nyl.nylim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NylimApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
